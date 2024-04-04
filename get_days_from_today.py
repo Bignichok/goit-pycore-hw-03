bfe0462631c3d4eea12430f78b1dcff2c8370a71
bignichok <inikita546@gmail.com>
2024-04-04 15:33:59 +0300

    add try/except for get_days_from_today function

--- get_days_from_today.py
+++ get_days_from_today.py
@@ -1,8 +1,12 @@
 from datetime import datetime
 
 def get_days_from_today(date: str) -> int:
-    timedelta = datetime.strptime(date, "%Y-%m-%d") - datetime.now()
-    return timedelta.days
+    try:
+        timedelta = datetime.strptime(date, "%Y-%m-%d") - datetime.now()
+        return timedelta.days
+    except ValueError:
+        print(f"date {date} should be in appropriate format YYYY-mm-dd")
 
 print(get_days_from_today("2021-10-09"))
 print(get_days_from_today("2027-10-09"))
+print(get_days_from_today("2027.10-09"))
