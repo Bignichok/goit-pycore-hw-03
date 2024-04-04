80152facf9708039c132fa6a49b4f4f8569e85c6
bignichok <inikita546@gmail.com>
2024-04-04 16:25:37 +0300

    add checks for get_numbers_ticket

--- get_numbers_ticket.py
+++ get_numbers_ticket.py
@@ -2,15 +2,20 @@ import random
 
 def get_numbers_ticket(min: int, max: int, quantity: int) -> list:
     result = []
-    while len(result) < quantity:
-        number = random.randint(min, max)
+    
+    if min > 0 and max <= 1000 and min <= quantity <= max:
+        while len(result) < quantity:
+            number = random.randint(min, max)
         
-        if number in result:
-            continue
-        else:
-            result.append(number)
+            if number in result:
+                continue
+            else:
+                result.append(number)
 
-    return sorted(result)
+        return sorted(result)
+    else:
+        return result
 
 print(get_numbers_ticket(1,1000,50))
 print(get_numbers_ticket(1,5,5))
+print(get_numbers_ticket(1,1000,5550))
